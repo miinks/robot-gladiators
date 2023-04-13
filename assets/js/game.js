cf53166d34a29a6816f7d6713809d5e090c1a604
Minks <jiyuniur@gmail.com>
2023-04-13 16:45:20 -0500

    test

---
+++ assets/js/game.js
@@ -0,0 +1,18 @@
+
+function fight() {
+    window.alert("The Fight has begun!")
+}
+fight();
+
+var playerName = window.prompt("What is your robot's name?");
+window.alert(playerName);
+
+console.log(playerName);
+console.log("Leave a String");
+console.log(10 + 10);
+console.log("Our Robot's name is "+ playerName);
+
+var name = "your name"
+console.log(name);
+
+//fight();
